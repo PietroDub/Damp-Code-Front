bfd7adccac132f8cdb27d6bd5abcc3b78a379fcb
pietrodub <pietroti453@gmail.com>
2026-04-05 22:49:00 -0300

    Implementação do header component

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -4,17 +4,37 @@ import "../style.css"
 const Header = () => {
   return (
     <header className="bg-fundo-claro h-20 w-full flex items-center justify-center">
-      <div className="w-10/12 flex items-center justify-between">
+      <div className="w-11/12 lg:w-10/12 flex items-center justify-between">
         <img src="/src/assets/DAMPCode.svg" className="md:max-w-3/12 pt-5" alt="damp-code-logo" />
-          <div className='hidden md:flex w-4/6 lg:w-3/6'>
-            <nav className='w-5/5 text-xl text-texto flex justify-between'>
+          {/* Modo large -> extra large */}
+          <div className='hidden lg:flex w-4/6 xl:w-4/7'>
+            <nav className='w-5/5 text-xl text-texto flex items-center justify-between'>
               <a href="">Explorar</a>
               <a href="">Ranking</a>
               <a href="">Para Empresas</a>
               <a href="">Entrar</a>
               <a href="">Criar Conta</a>
             </nav>
-            <div className=''>
+            <div className='flex h-2/3 gap-x-5 lg:pl-5'>
+                <img src="/src/assets/portugues.svg" alt="" />
+                <img src="/src/assets/ingles.svg" alt="" />
+                <img src="/src/assets/espanhol.svg" alt="" />                
+            </div>
+          </div>
+
+          {/* Modo mobile -> medium */}
+          <div className='hidden md:flex lg:hidden'>
+            <nav className='w-5/6 text-lg text-texto flex items-center justify-center gap-x-3'>
+              <a href="">Explorar</a>
+              <a href="">Ranking</a>
+              <a href="">Para Empresas</a>
+              <a href="">Entrar</a>
+              <a href="">Criar Conta</a>
+            </nav>
+            <div className='flex gap-x-5 w-1/4 items-center justify-center'>
+                <img className="h-3/4" src="/src/assets/portugues.svg" alt="" />
+                <img className="h-3/4" src="/src/assets/ingles.svg" alt="" />
+                <img className="h-3/4" src="/src/assets/espanhol.svg" alt="" />                
             </div>
           </div>
       </div>
